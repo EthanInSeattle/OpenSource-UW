110412009993a7fdba4c5db0a5233427ca56b398
Gihwan Kwon <gihwankwon@Gihwanui-MacBook-Pro.local>
2017-12-26 21:20:23 -0800

    changed a bit more

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kangwooc/Desktop/OpenSource-UW/venv"
+set -gx VIRTUAL_ENV "/Users/gihwankwon/Desktop/OpenSource-UW/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
